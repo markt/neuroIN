8c2954371d7cdd68998026e993f5387198c8f577
markt <powdergnomes@gmail.com>
2021-08-13 21:48:43 -0600

    added setup.py in addition to setup.cfg

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='neuroIN',
+   version='0.0.1',
+   author='Mark Taylor',
+   author_email='mark.a.taylor.gr@dartmouth.edu',
+   packages=find_packages(where="src"),
+   scripts=[],
+   url='https://github.com/markt/neuroin',
+   license='LICENSE',
+   description='neuroIN is a deep learning package for neuroimaging data',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "pytest",
+   ],
+)
